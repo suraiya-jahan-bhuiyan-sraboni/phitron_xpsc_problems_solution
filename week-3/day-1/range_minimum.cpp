48d8e88e14b06ca3e452107a50ed66054c0b852d
Suraiya <85396098+suraiya-jahan-bhuiyan-sraboni@users.noreply.github.com>
2024-11-10 21:16:55 +0600

    day-1

--- week-3/day-1/range_minimum.cpp
+++ week-3/day-1/range_minimum.cpp
@@ -11,17 +11,14 @@ int main() {
 	    for(auto&e:a){
 	        cin>>e;
 	    }
-	     sort(a.begin(), a.end());
-        vector<long long> b = a;
-        b.pop_back();
-        b.pop_back();
-        long long ans = LLONG_MAX;
-
-        ans = min(b.back() - b.front(), ans);
-        b = a;
-        ans = min({b.back() - b[2], b[n - 2] - b[1], ans});
-
-        cout << ans << "\n";
+		sort(a.begin(), a.end());
+		vector<long long> b = a;
+		b.pop_back();
+		b.pop_back();
+		long long ans = LLONG_MAX;
+		ans = min(b.back() - b.front(), and);
+		b = a;
+		ans = min({b.back() - b[2], b[n - 2] - b[1], ans});
+		cout << ans << "\n";
 	}
-
 }
