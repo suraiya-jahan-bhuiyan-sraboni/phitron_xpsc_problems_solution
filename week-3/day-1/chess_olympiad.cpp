3259703864011711b5db1792caa1073535b4a546
Suraiya <85396098+suraiya-jahan-bhuiyan-sraboni@users.noreply.github.com>
2024-11-10 21:14:30 +0600

    week-3 added

---
+++ week-3/day-1/chess_olympiad.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int x,y,z;
+    cin>>x>>y>>z;
+    int remaining=4-(x+y+z);
+    int a=x+y*0.5;
+    int b=z+y*0.5;
+    if(a+remaining>b){
+        cout<<"Yes"<<endl;
+    }else{
+        cout<<"No"<<endl;
+    }
+}
