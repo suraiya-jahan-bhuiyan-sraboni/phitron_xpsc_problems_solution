8f49b3750943914581c0f83c92cf8b8f4ebed992
suraiya-jahan-bhuiyan-sraboni <suraiya.jaha.bhuiyan.12@gmail.com>
2024-11-17 23:09:06 +0600

    week-4 day-1

---
+++ week-4/day-1/RCB_vs_CSK.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x,y;
+    cin>>x>>y;
+    if(x-y>=18){
+        cout<<"RCB"<<endl;
+    }else{
+        cout<<"CSK"<<endl;
+    }
+
+    return 0;
+}
